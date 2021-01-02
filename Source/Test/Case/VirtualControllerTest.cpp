9c78a5ad98ac7e5f092573afbe7c1a578cb12772
Samuel Grossman <samuelgr@users.noreply.github.com>
2021-01-02 14:44:00 -0800

    Added a mock XInput interface to be used for testing virtual controller objects.

--- Source/Test/Case/VirtualControllerTest.cpp
+++ Source/Test/Case/VirtualControllerTest.cpp
@@ -14,10 +14,14 @@
 #include "ControllerTypes.h"
 #include "TestCase.h"
 #include "VirtualController.h"
+#include "XInputInterface.h"
 
 #include <cmath>
 #include <cstdint>
+#include <deque>
 #include <memory>
+#include <optional>
+#include <xinput.h>
 
 
 namespace XidiTest
@@ -37,7 +41,134 @@ namespace XidiTest
     static const Mapper kTestSingleAxisMapper({
         .stickLeftX = std::make_unique<AxisMapper>(kTestSingleAxis)
     });
+
+
+    // -------- INTERNAL TYPES --------------------------------------------- //
+
+    /// Mock version the XInput interface, used for test purposes to provide fake XInput data to a virtual controller.
+    class MockXInput : public IXInput
+    {
+    public:
+        // -------- TYPE DEFINITIONS --------------------------------------- //
+
+        /// Defines the behavior of a mock method call.
+        /// @tparam OutputObjectType Type of method output parameter.
+        template <typename OutputObjectType> struct SMethodCallSpec
+        {
+            DWORD returnCode;                                               ///< Desired return code.
+            std::optional<OutputObjectType> maybeOutputObject;              ///< Desired output object. If absent, no object is copied to the output parameter.
+            int repeatTimes;                                                ///< Number of times the call should be repeated before it is removed. Zero means the call should happen exactly once.
+        };
+
     
+    private:
+        // -------- INSTANCE VARIALBES ------------------------------------- //
+
+        /// Expected user index. All calls will fail if they do not match.
+        const DWORD kUserIndex;
+
+        /// Expected behavior for calls to #GetCapabilities.
+        std::deque<SMethodCallSpec<XINPUT_CAPABILITIES>> callsGetCapabilities;
+
+        /// Expected behavior for calls to #GetState.
+        std::deque<SMethodCallSpec<XINPUT_STATE>> callsGetState;
+
+
+    public:
+        // -------- CONSTRUCTION AND DESTRUCTION --------------------------- //
+
+        /// Initialization constructor.
+        /// Requires an XInput user index.
+        MockXInput(DWORD kUserIndex) : kUserIndex(kUserIndex), callsGetCapabilities(), callsGetState()
+        {
+            // Nothing to do here.
+        }
+
+
+    private:
+        // -------- CLASS METHODS ------------------------------------------ //
+
+        /// Performs a mock method call.
+        /// @tparam OutputObjectType Type of method output parameter.
+        /// @param [in] methodName Method name to use when generating error messages.
+        /// @param [in,out] callSpecs Ordered list of method call specifications, which is modified during this call as appropriate.
+        /// @param [out] outputBuf If the call specification includes an output object, this buffer is filled with the contents of that object.
+        /// @return Return code specified in the call specification.
+        template <typename OutputObjectType> static DWORD DoMockMethodCall(const wchar_t* methodName, std::deque<SMethodCallSpec<OutputObjectType>>& callSpecs, OutputObjectType* outputBuf)
+        {
+            if (callSpecs.empty())
+                TEST_FAILED_BECAUSE(L"%s: unexpected method call.", methodName);
+            
+            SMethodCallSpec<OutputObjectType>& callSpec = callSpecs.front();
+            const DWORD returnCode = callSpec.returnCode;
+
+            if (callSpec.maybeOutputObject.has_value())
+            {
+                const OutputObjectType outputObject = callSpec.maybeOutputObject.value();
+                memcpy(outputBuf, &outputObject, sizeof(outputObject));
+            }
+
+            if (0 == callSpec.repeatTimes)
+                callSpecs.pop_front();
+            else
+                callSpec.repeatTimes -= 1;
+
+            return returnCode;
+        }
+
+
+    public:
+        // -------- INSTANCE METHODS --------------------------------------- //
+
+        /// Submits an expected call for the #GetCapabilities method.
+        /// @param [in] callSpec Specifications that describe the desired behavior of the call.
+        inline void ExpectCallGetCapabilities(const SMethodCallSpec<XINPUT_CAPABILITIES>& callSpec)
+        {
+            callsGetCapabilities.push_back(callSpec);
+        }
+
+        /// Submits an expected call for the #GetState method.
+        /// @param [in] callSpec Specifications that describe the desired behavior of the call.
+        inline void ExpectCallGetState(const SMethodCallSpec<XINPUT_STATE>& callSpec)
+        {
+            callsGetState.push_back(callSpec);
+        }
+
+
+        // -------- CONCRETE INSTANCE METHODS ------------------------------ //
+
+        DWORD GetCapabilities(DWORD dwUserIndex, DWORD dwFlags, XINPUT_CAPABILITIES* pCapabilities) override
+        {
+            if (kUserIndex != dwUserIndex)
+                TEST_FAILED_BECAUSE(L"XInputGetCapabilities: user index mismatch (expected %u, got %u).", kUserIndex, dwUserIndex);
+            else if (dwUserIndex >= XUSER_MAX_COUNT)
+                TEST_FAILED_BECAUSE(L"XInputGetCapabilities: user index too large (%u versus maximum %u).", dwUserIndex, XUSER_MAX_COUNT);
+            
+            switch (dwFlags)
+            {
+            case 0:
+            case XINPUT_FLAG_GAMEPAD:
+                break;
+                
+            default:
+                TEST_FAILED_BECAUSE(L"XInputGetCapabilities: unsupported flags (0x%08x).", dwFlags);
+            }
+
+            return DoMockMethodCall(L"XInputGetCapabilities", callsGetCapabilities, pCapabilities);
+        }
+
+        DWORD GetState(DWORD dwUserIndex, XINPUT_STATE* pState) override
+        {
+            if (kUserIndex != dwUserIndex)
+                TEST_FAILED_BECAUSE(L"XInputGetState: user index mismatch (expected %u, got %u).", kUserIndex, dwUserIndex);
+            else if (dwUserIndex >= XUSER_MAX_COUNT)
+                TEST_FAILED_BECAUSE(L"XInputGetState: user index too large (%u versus maximum %u).", dwUserIndex, XUSER_MAX_COUNT);
+
+            return DoMockMethodCall(L"XInputGetState", callsGetState, pState);
+        }
+    };
+
+
     // -------- INTERNAL FUNCTIONS ----------------------------------------- //
 
     /// Computes and returns the deadzone value that corresponds to the specified percentage of an axis' physical range of motion.
