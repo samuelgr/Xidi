e1b17daaaf3ff6e7ac97b3347d87052b90ab5c41
Samuel Grossman <samuelgr@users.noreply.github.com>
2022-01-15 21:52:56 -0800

    Simplified some force feedback effect object creation logic.

--- Source/VirtualDirectInputDevice.cpp
+++ Source/VirtualDirectInputDevice.cpp
@@ -896,23 +896,14 @@ namespace Xidi
             // If parameters are provided they need to be complete.
             // This method does not provide any way of specifying flags to restrict the parameters that are set.
             // However, for compatibility with older versions of DirectInput 5 it is necessary to check the structure size here to avoid having the method read past the end of the valid parameter buffer.
-            const DWORD kParameterFlags = ((sizeof(DIEFFECT_DX5) == lpeff->dwSize) ? DIEP_ALLPARAMS_DX5 : DIEP_ALLPARAMS);
-            switch (newEffect->SetParametersInternal(lpeff, kParameterFlags | DIEP_NODOWNLOAD))
-            {
-            case DI_DOWNLOADSKIPPED:
-                break;
-
-            default:
-                LOG_INVOCATION_AND_RETURN(DIERR_INVALIDPARAM, kMethodSeverity);
-            }
-
             // Success of this method does not depend on whether or not a download completed successfully.
             // If it failed because the device is full, then the effect can still exist even if it is not physically on the device.
             // Likewise, if the device is not exclusively acquired, then the device just needs to be acquired before the effect can be downloaded.
-            switch (newEffect->DownloadInternal())
+            const DWORD kParameterFlags = ((sizeof(DIEFFECT_DX5) == lpeff->dwSize) ? DIEP_ALLPARAMS_DX5 : DIEP_ALLPARAMS);
+            switch (newEffect->SetParametersInternal(lpeff, kParameterFlags))
             {
             case DI_OK:
-            case DIERR_DEVICEFULL:
+            case DI_DOWNLOADSKIPPED:
             case DIERR_NOTEXCLUSIVEACQUIRED:
                 break;
 
